a43a78b01593092c62045f328829238122704786
gordanidmit <gordanidmit@ya.ru>
2015-03-19 01:22:55 +0300

    maked clock

--- Терминал андроид/App5/MainActivity.cs
+++ Терминал андроид/App5/MainActivity.cs
@@ -6,16 +6,35 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 using Android.Content.PM;
+using System.Threading.Tasks;
 
 namespace App5
 {
     [Activity(Label = "App5", MainLauncher = true, Icon = "@drawable/icon", Theme = "@android:style/Theme.Light.NoTitleBar.Fullscreen", ConfigurationChanges = ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.SensorLandscape)]
     public class MainActivity : Activity
     {
+        TextView watch;
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
             SetContentView(Resource.Layout.WatchQueue);
+
+            watch = (TextView)FindViewById(Resource.Id.textClock);
+        }
+        protected override void OnResume()
+        {
+            timerLoop();
+            base.OnResume();
+        }
+        async void timerLoop()
+        {
+            while (true)
+            {
+                await Task.Delay(1000);
+                DateTime NowTime = DateTime.Now;
+                string NowTimeStr = NowTime.ToString("HH:mm:ss");
+                watch.Text = "Время:\n" + NowTimeStr;
+            }
         }
     }
 }
