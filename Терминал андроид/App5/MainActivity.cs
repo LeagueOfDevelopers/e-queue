2a601858a748fb05776be851e3309106742a2195
gordanidmit <gordanidmit@ya.ru>
2015-03-19 04:43:53 +0300

    LoadingConfig
    загрузка ip и port из файла

--- Терминал андроид/App5/MainActivity.cs
+++ Терминал андроид/App5/MainActivity.cs
@@ -7,6 +7,8 @@ using Android.Widget;
 using Android.OS;
 using Android.Content.PM;
 using System.Threading.Tasks;
+using System.IO;
+using System.Net;
 
 namespace App5
 {
@@ -14,12 +16,19 @@ namespace App5
     public class MainActivity : Activity
     {
         TextView watch;
+
+        IPAddress ip;
+        int port;
+
+        AlertDialog.Builder dialog;
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
             SetContentView(Resource.Layout.WatchQueue);
 
             watch = (TextView)FindViewById(Resource.Id.textClock);
+            checkConfig();
         }
         protected override void OnResume()
         {
@@ -34,6 +43,39 @@ namespace App5
                 DateTime NowTime = DateTime.Now;
                 string NowTimeStr = NowTime.ToString("HH:mm:ss");
                 watch.Text = "Время:\n" + NowTimeStr;
+            }
+        }
+        void checkConfig()
+        {
+            var folderPath = Android.OS.Environment.GetExternalStoragePublicDirectory(Android.OS.Environment.DataDirectory.ToString()) +"/Terminal";
+            var filePath = Android.OS.Environment.GetExternalStoragePublicDirectory(Android.OS.Environment.DataDirectory.ToString()) + "/Terminal/ipconfig";
+            if (!File.Exists(filePath))
+            {
+                if (!Directory.Exists(folderPath))
+                    Directory.CreateDirectory(folderPath);
+                File.Create(filePath);
+                dialog = new AlertDialog.Builder(this);
+                dialog.SetTitle("IpConfig");
+                dialog.SetMessage(String.Format("Создан файл конфигурации ({0}). Пожалуйста заполните его и перезапустите приложениею", filePath));
+                dialog.SetPositiveButton("OK", delegate { this.Finish(); });
+                dialog.Show();
+            }
+            else
+            {
+                StreamReader sr = new StreamReader(filePath);
+                try
+                {
+                    ip = IPAddress.Parse(sr.ReadLine());
+                    port = int.Parse(sr.ReadLine());
+                }
+                catch
+                {
+                    dialog = new AlertDialog.Builder(this);
+                    dialog.SetTitle("IpConfigError");
+                    dialog.SetMessage(String.Format("Информация в файле конфигурации ({0:s}) некорректна", filePath));
+                    dialog.Show();
+                }
+                sr.Close();
             }
         }
     }
