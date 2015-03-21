72048cbda6d90db6ea4c6a1fd42da1b0c3a10d4e
gordanidmit <gordanidmit@ya.ru>
2015-03-21 04:55:27 +0300

    Connecting to serv
    отлажено подключение, добавлен timeout

--- Терминал андроид/App5/MainActivity.cs
+++ Терминал андроид/App5/MainActivity.cs
@@ -9,16 +9,21 @@ using Android.Content.PM;
 using System.Threading.Tasks;
 using System.IO;
 using System.Net;
+using System.Net.Sockets;
+using System.Threading;
 
 namespace App5
 {
+    enum requests { prop1, prop2, prop3, prop4, prop5, prop6, EOSes, GetBD, GetHD, GetCl, SetCg, GetCg, enumErr }
     [Activity(Label = "App5", MainLauncher = true, Icon = "@drawable/icon", Theme = "@android:style/Theme.Light.NoTitleBar.Fullscreen", ConfigurationChanges = ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.SensorLandscape)]
     public class MainActivity : Activity
     {
         TextView watch;
 
-        IPAddress ip;
-        int port;
+        IPEndPoint ipEndPoint;
+        Socket SSender;
+        IAsyncResult ar;
+        bool q = false; // переменная, надо ли проверять результат подключения
 
         AlertDialog.Builder dialog;
 
@@ -28,24 +33,37 @@ namespace App5
             SetContentView(Resource.Layout.WatchQueue);
 
             watch = (TextView)FindViewById(Resource.Id.textClock);
-            checkConfig();
+            if(checkConfig())
+            {
+                Connecting();
+            }
         }
         protected override void OnResume()
         {
-            timerLoop();
+            Loop();
             base.OnResume();
         }
-        async void timerLoop()
+        async void Loop()
         {
             while (true)
             {
-                await Task.Delay(1000);
+                await Task.Delay(100);
                 DateTime NowTime = DateTime.Now;
                 string NowTimeStr = NowTime.ToString("HH:mm:ss");
                 watch.Text = "Время:\n" + NowTimeStr;
+                if(q)
+                {
+                    if (SSender.Connected)
+                        ShowMessage("Connection", "Соединение установлено", false);
+                    else
+                    {
+                        ShowMessage("Connection", "Не удалось установить соединение", true);
+                    }
+                    q = false;
+                }
             }
         }
-        void checkConfig()
+        bool checkConfig()
         {
             var folderPath = Android.OS.Environment.GetExternalStoragePublicDirectory(Android.OS.Environment.DataDirectory.ToString()) +"/Terminal";
             var filePath = Android.OS.Environment.GetExternalStoragePublicDirectory(Android.OS.Environment.DataDirectory.ToString()) + "/Terminal/ipconfig";
@@ -54,29 +72,65 @@ namespace App5
                 if (!Directory.Exists(folderPath))
                     Directory.CreateDirectory(folderPath);
                 File.Create(filePath);
-                dialog = new AlertDialog.Builder(this);
-                dialog.SetTitle("IpConfig");
-                dialog.SetMessage(String.Format("Создан файл конфигурации ({0}). Пожалуйста заполните его и перезапустите приложениею", filePath));
-                dialog.SetPositiveButton("OK", delegate { this.Finish(); });
-                dialog.Show();
+                ShowMessage("IpConfig", String.Format("Создан файл конфигурации ({0}). Пожалуйста заполните его и перезапустите приложение", filePath), true);
+                return false;
             }
             else
             {
                 StreamReader sr = new StreamReader(filePath);
                 try
                 {
-                    ip = IPAddress.Parse(sr.ReadLine());
-                    port = int.Parse(sr.ReadLine());
+                    string str = sr.ReadLine();
+                    IPAddress ip = IPAddress.Parse(str);
+                    int port = int.Parse(sr.ReadLine());
+                    ipEndPoint = new IPEndPoint(ip, port);
+                    sr.Close();
+                    return true;
                 }
                 catch
                 {
-                    dialog = new AlertDialog.Builder(this);
-                    dialog.SetTitle("IpConfigError");
-                    dialog.SetMessage(String.Format("Информация в файле конфигурации ({0:s}) некорректна", filePath));
-                    dialog.Show();
+                    ShowMessage("IpConfigError", String.Format("Информация в файле конфигурации ({0:s}) некорректна", filePath), false);
+                    sr.Close();
+                    return false;
                 }
-                sr.Close();
             }
+        }
+        void Connecting()
+        {
+            try
+            {
+                SSender = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                SSender.BeginConnect(ipEndPoint, new AsyncCallback(callback), SSender);
+                new Thread(new ParameterizedThreadStart(timeout)).Start();
+            }
+            catch (SocketException exc)
+            {
+                ShowMessage("ConnectionError", String.Format("Ошибка подключения: {0}", exc.Message), false);
+            }
+
+        }
+        void callback(IAsyncResult r)
+        {
+            ar = r;
+            q = true;
+        }
+        void timeout(object ob)
+        {
+            Thread.Sleep(TimeSpan.FromSeconds(15));
+            if (ar==null||!ar.IsCompleted)
+                q = true;
+        }
+        void ShowMessage(string title, string message, bool exit)
+        {
+            dialog = new AlertDialog.Builder(this);
+            dialog.SetTitle(title);
+            dialog.SetMessage(message);
+            dialog.SetPositiveButton("OK", delegate
+            {
+                if (exit)
+                    this.Finish();
+            });
+            dialog.Show();
         }
     }
 }
